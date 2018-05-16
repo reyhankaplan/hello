c8b6cb2f705c36dd4429bb07faf06dc1c2d54fa5
Reyhan Ünal <rryhn.96@gmail.com>
2018-05-16 17:23:52 +0300

    fix read queries, add search function

--- src/read.js
+++ src/read.js
@@ -1,11 +1,51 @@
-cont db = require('./db')
+const db = require('./db')
 
 const QUERIES = {
-	get_non_friends = "select id from person where id not in \
-					(select friend_id from friendship where person_id='$1') and \
-					id not in (select person_id from friendship where friend_id='$1') \
-					and id!='$1'",
-	get_friends = 'select friend_id from friendship where person_id=$1',
-	get_people_for_search = "select * from person where name ~* '$1' or id='$2'"
+	get_non_friends: "select id from person where id not in \
+					(select friend_id from friendship where person_id=$1) and \
+					id not in (select person_id from friendship where friend_id=$1) \
+					and id!=$1",
+	get_friends: 'select friend_id from friendship where person_id=$1',
+	get_people_for_search: "select * from person where name ~* $2 or id=$1"
 }
 // get_people_for_search sorgusunda isim girilmemisse $1 yerine - at
+
+// Bu fonksiyon isme veya id'ye gore veritabanindan kisileri alip dondurur
+async function searchForPeople(personId='', personName='-') {
+
+	let searchResults = []
+
+	await 
+	db(
+		{
+			text:QUERIES.get_people_for_search, 
+			values:[personId, personName]
+		}
+		).then(res => {
+
+			if(res.rowCount > 0) {
+
+				searchResults = res.rows
+
+			}
+			
+			console.log(res.rows)
+			console.log(`Row count: ${res.rowCount}... first row:${res.rows[0]}`)
+			
+		}).catch(err => {
+			console.log(err)
+		})
+
+	return searchResults
+}
+/*
+
+// searchForPeople kullanimi
+searchForPeople('', 'can').then(res => {
+
+	res.forEach(e => {
+		
+		console.log(`${e.id} - ${e.name}`)
+	})
+})
+*/
