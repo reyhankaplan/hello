3cca2e6fa78ad4fb72e746c735b6d353a39f25f8
Reyhan Ünal <rryhn.96@gmail.com>
2018-05-17 02:17:20 +0300

    add new read functions and queries

--- src/read.js
+++ src/read.js
@@ -6,12 +6,14 @@ const QUERIES = {
 					id not in (select person_id from friendship where friend_id=$1) \
 					and id!=$1",
 	get_friends: 'select friend_id from friendship where person_id=$1',
-	get_people_for_search: "select * from person where name ~* $2 or id=$1"
+	get_people_for_search: "select * from person where name ~* $2 or id=$1",
+	get_profile: "select * from profile where id=$1",
+	get_person: "select * from person where id=$1",
 }
 // get_people_for_search sorgusunda isim girilmemisse $1 yerine - at
 
 // Bu fonksiyon isme veya id'ye gore veritabanindan kisileri alip dondurur
-async function searchForPeople(personId='', personName='-') {
+async function searchInPeople(personId='', personName='-') {
 
 	let searchResults = []
 
@@ -29,8 +31,8 @@ async function searchForPeople(personId='', personName='-') {
 
 			}
 			
-			console.log(res.rows)
-			console.log(`Row count: ${res.rowCount}... first row:${res.rows[0]}`)
+			// console.log(res.rows)
+			// console.log(`Row count: ${res.rowCount}...`)
 			
 		}).catch(err => {
 			console.log(err)
@@ -40,8 +42,8 @@ async function searchForPeople(personId='', personName='-') {
 }
 /*
 
-// searchForPeople kullanimi
-searchForPeople('', 'can').then(res => {
+// searchInPeople kullanimi
+searchInPeople('', 'can').then(res => {
 
 	res.forEach(e => {
 		
@@ -49,3 +51,143 @@ searchForPeople('', 'can').then(res => {
 	})
 })
 */
+
+// Id'si verilen kisinin arkadaslarini ve arkadasi olmayanlari getiren fonksiyon
+async function getRelatedPeople(personId) {
+	let friends = []
+	let nonfriends = []
+
+	await 
+	db(
+		{
+			text:QUERIES.get_friends, 
+			values:[personId]
+		}
+		).then(res => {
+
+			if(res.rowCount > 0) {
+
+				friends = res.rows
+
+			}
+			
+			// console.log(res.rows)
+			// console.log(`Row count: ${res.rowCount}...`)
+			
+		}).catch(err => {
+			console.log(err)
+		})
+
+	await 
+	db(
+		{
+			text:QUERIES.get_non_friends, 
+			values:[personId]
+		}
+		).then(res => {
+
+			if(res.rowCount > 0) {
+
+				nonfriends = res.rows
+
+			}
+			
+			// console.log(res.rows)
+			// console.log(`Row count: ${res.rowCount}...`)
+			
+		}).catch(err => {
+			console.log(err)
+		})
+
+	return {friends, nonfriends}
+}
+/*
+getRelatedPeople('2014123024').then(res => {
+
+	res.friends.forEach(e => {
+
+		console.log(`Hello, my friend... ${e.friend_id}`)	
+	})
+	
+	res.nonfriends.forEach(e => {
+
+		console.log(`Hello, man... ${e.id}`)	
+	})
+})
+*/
+
+async function getProfile(personId) {
+
+	let profile = {id: personId, values:[]}
+
+	await 
+	db(
+		{
+			text:QUERIES.get_profile, 
+			values:[personId]
+		}
+		).then(res => {
+
+			if(res.rowCount > 0) {
+
+				
+				let obj = Object.assign({}, res.rows[0])
+
+				delete obj.id
+
+				for(let property in obj) {
+
+					profile.values.push(obj[property])
+					// console.log(`Pushed property: ${property}`)
+				}
+			}
+			
+			// console.log(res.rows[0])
+			// console.log(`Row count: ${res.rowCount}...`)
+			
+		}).catch(err => {
+			console.log(err)
+		})	
+
+	return profile
+}
+/*
+getProfile('2014123024').then(res => {
+
+	console.log(res)
+})
+*/
+
+async function getPerson(personId) {
+
+	let person
+	
+	await 
+	db(
+		{
+			text:QUERIES.get_person, 
+			values:[personId]
+		}
+		).then(res => {
+
+			if(res.rowCount > 0) {
+
+				person = res.rows[0]
+
+			}
+			
+			// console.log(res.rows)
+			// console.log(`Row count: ${res.rowCount}...`)
+			
+		}).catch(err => {
+			console.log(err)
+		})
+
+	return person
+}
+/*
+getPerson('2014123024').then(res => {
+	console.log(res)
+})
+*/
+module.exports = { searchInPeople, getRelatedPeople, getProfile, getPerson }
