9ce9d78201b441758b14d8478eb396d0565575cd
Reyhan Ünal <rryhn.96@gmail.com>
2018-05-16 10:04:16 +0300

    edit read query

--- src/read.js
+++ src/read.js
@@ -1,11 +1,10 @@
+cont db = require('./db')
+
 const QUERIES = {
 	get_non_friends = "select id from person where id not in \
 					(select friend_id from friendship where person_id='$1') and \
 					id not in (select person_id from friendship where friend_id='$1') \
-					and id!='$1' limit (select count(*)/2 as c from person where \
-					id not in (select friend_id from friendship where person_id='$1') \
-					and id not in (select person_id from friendship where friend_id='$1') \
-					and id != '$1')",
+					and id!='$1'",
 	get_friends = 'select friend_id from friendship where person_id=$1',
 	get_people_for_search = "select * from person where name ~* '$1' or id='$2'"
 }
