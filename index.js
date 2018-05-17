f1ebad4cb1e76f33e9a41172ab954648106ba89c
Reyhan Ünal <rryhn.96@gmail.com>
2018-05-17 15:57:09 +0300

    make little api

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express')
 
+const findSuggestions = require('./src/hello')
+const { searchInPeople } = require('./src/read.js')
+
 const app = express()
 
 app.get('/person', (req, res) => {
-	res.send(`Hello ${req.query.name}!`)
+
+	let id = req.query.id || ''
+	let name = req.query.name || '-'
+
+	searchInPeople(id, name).then(r => {
+
+		if(r.message) {
+			res.send(r.message)
+		}else {
+			res.send(r)
+		}
+
+	})
+
+})
+
+app.get('/hello', (req, res) => {
+	
+	if(!req.query.id) {
+		res.end('missing person id')
+	}
+	console.log(req.query.id)
+
+	let iterCount = req.query.iter || 10000
+	let stepSize = req.query.step || 0.001
+
+	console.log(`Step size: ${stepSize}\nIteration count: ${iterCount}`)
+
+	findSuggestions(req.query.id, iterCount, stepSize).then(r => {
+
+		console.log(r)
+		res.send(r)
+	})
 })
 
 app.listen(3000)
